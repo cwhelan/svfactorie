45880931e4892178685a5855743b3fade74a9a32
Chris Whelan <cwhelan@gmail.com>
2013-07-19 11:09:28 -0700

    initial hello world project setup

---
+++ src/main/scala/helloworld/HelloWorld.scala
@@ -0,0 +1,11 @@
+package helloworld
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: cwhelan
+ * Date: 7/19/13
+ * Time: 10:24 AM
+ */
+object HelloWorld extends App {
+  println("Hello World")
+}
---
+++ src/test/scala/helloworld/HelloWorldTest.scala
@@ -0,0 +1,16 @@
+package helloworld
+
+import org.scalatest.{FunSuite, FunSpec}
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: cwhelan
+ * Date: 7/19/13
+ * Time: 10:27 AM
+ */
+class HelloWorldTest extends FunSuite {
+
+  test("hello test") {
+    assert(true);
+  }
+}
