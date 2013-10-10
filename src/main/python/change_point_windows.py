a416507f3b714c5968d9010d06e8c148e7750fe8
Chris Whelan <cwhelan@gmail.com>
2013-10-10 00:12:00 -0700

    parameterize change point detection so it can be used for other data types

--- src/main/python/change_point_windows.py
+++ src/main/python/change_point_windows.py
@@ -5,6 +5,9 @@ import gzip
 import changePointDetection
 
 window_file = sys.argv[1]
+data_file_suffix = sys.argv[2]
+obs_cutoff = int(sys.argv[3])
+segment_size = int(sys.argv[4])
 
 current_seg_file = None
 fp = None
@@ -13,9 +16,9 @@ for line in open(window_file, "r"):
     chrom = fields[0]
     start = fields[1]
     end = fields[2]
-    segFile = "%s.inserts.txt.gz" % chrom
+    segFile = "%s.%s" % (chrom, data_file_suffix)
     if segFile != current_seg_file:
         fp = gzip.open(segFile)
         current_seg_file = segFile
-    changePointDetection.detect_change_points(int(start) - 125, int(end) + 100, fp, 100, 7, sys.stdout, printBed=True, chrom=chrom, resolution=25)
+    changePointDetection.detect_change_points(int(start) - (segment_size + 25), int(end) + segment_size, fp, segment_size, obs_cutoff, sys.stdout, printBed=True, chrom=chrom, resolution=25)
 
